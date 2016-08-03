412a4a5c26d67acc48d286f5e79ec87733f50675
Qing Chang <connor@petabi.com>
2016-08-03 12:03:25 -0700

    Apply boost 1.6 patch to update from version 1.55

--- devel/boost-all/compiled.mk
+++ devel/boost-all/compiled.mk
@@ -1,45 +1,49 @@
 USE_LDCONFIG=	yes
 PKGMESSAGE=	${WRKDIR}/pkg-message
-BJAM=		bjam
-USES+=		compiler:c++11-lang
+MAKE_CMD=	bjam
+MAKEFILE=	#
+MAKE_FLAGS=	#
+ALL_TARGET=	stage
+USES+=		compiler:features
 
 PLIST_SUB+=	BOOST_SHARED_LIB_VER=${PORTVERSION} COMPAT_LIB_VER=5
 
 PKG_MESSAGE_FILE_THREADS=	${PORTSDIR}/devel/boost-all/pkg-message.threads
 PKG_MESSAGE_FILE_PYTHON=	${PORTSDIR}/devel/boost-all/pkg-message.python
 
-BJAM_ARGS=	--layout=system \
+MAKE_ARGS=	--layout=system \
 		--prefix=${PREFIX} \
 
 # Our compiler-flags will be added AFTER those set by bjam. We remove
 # the optimization level, because Boost sets it itself (to -O3 in case
 # of gcc/g++):
-BJAM_ARGS+=    cxxflags="${CXXFLAGS:N-O*}" cflags="${CFLAGS:N-O*}"
+MAKE_ARGS+=    cxxflags="${CXXFLAGS:N-O*}" cflags="${CFLAGS:N-O*}"
 
 BOOST_TOOLSET=	${CHOSEN_COMPILER_TYPE}
 
-BJAM_ARGS+=	--toolset=${BOOST_TOOLSET} \
+MAKE_ARGS+=	--toolset=${BOOST_TOOLSET} \
 		${_MAKE_JOBS}
 
-VERBOSE_BUILD_VARS=	BJAM_ARGS+=-d2
+VERBOSE_BUILD_VARS=	MAKE_ARGS+=-d2
 
-DEBUG_VARS=	BJAM_ARGS+=debug
-DEBUG_VARS_OFF=	BJAM_ARGS+=release
+DEBUG_VARS=	MAKE_ARGS+=debug
+DEBUG_VARS_OFF=	MAKE_ARGS+=release
 
-BJAM_ARGS+=	threading=multi \
+MAKE_ARGS+=	threading=multi \
 		link=shared,static
 
-BJAM_ARGS+=	optimization=speed
-OPTIMIZED_CFLAGS_VARS=	BJAM_ARGS+=inlining=full
+MAKE_ARGS+=	optimization=speed
+OPTIMIZED_CFLAGS_VARS=	MAKE_ARGS+=inlining=full
 
 # ccache build fails when using precompiled headers, on a cached build.
-.if defined(WITH_CCACHE_BUILD)
-BJAM_ARGS+=	pch=off
-.endif
+#
+# base gcc 4.2.1 fails when using precompiled headers and running with a
+# recent FreeBSD 11 kernel
+MAKE_ARGS+=	pch=off
 
 post-patch:
 .if defined(USE_BINUTILS)
-	@${ECHO} "using ${BOOST_TOOLSET} : : ${CXX} : <linkflags>-B${LOCALBASE}/bin ;" >> ${WRKSRC}/tools/build/v2/user-config.jam
+	@${ECHO} "using ${BOOST_TOOLSET} : : ${CXX} : <linkflags>-B${LOCALBASE}/bin ;" >> ${WRKSRC}/tools/build/src/user-config.jam
 .else
-	@${ECHO} "using ${BOOST_TOOLSET} : : ${CXX} ;" >> ${WRKSRC}/tools/build/v2/user-config.jam
+	@${ECHO} "using ${BOOST_TOOLSET} : : ${CXX} ;" >> ${WRKSRC}/tools/build/src/user-config.jam
 .endif
